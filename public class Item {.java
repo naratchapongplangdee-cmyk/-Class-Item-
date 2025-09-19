585775a8d3eb93ab0324d53d25ef359c045cb453
Naratchapong 1670701075 <naratchapongplangdee@gmail.com>
2025-09-19 09:43:31 +0700

    Add files via upload

---
+++ public class Item {.java
@@ -0,0 +1,15 @@
+public class Item {
+    private float price;
+
+    public Item() {
+        this.price = 0;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public void setPrice(float p) {
+        this.price = p;
+    }
+}
